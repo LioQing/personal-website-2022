2e4e76fd9dbd9371bf08b6597ae7447bb53e63a8
LioQing <46854695+LioQing@users.noreply.github.com>
2023-09-01 11:51:55 +0800

    Add GA

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-2R4YG58N6L");
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
